8afc8bf8c82e4dff466a80f8e4486a6aec579cd8
Ryan Wong <ryanwong6265@gmail.com>
2025-01-11 22:06:39 -0500

    Removed onBlur

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,6 +5,7 @@ function InputField({ game } : { game: string }) {
   const [query, setQuery] = useState<string>('');
   const [matches, setMatches] = useState<string[]>([]);
   const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [noMatches, setNoMatches] = useState<boolean>(true);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const gameToData: Record<string, any> = {
@@ -34,25 +35,24 @@ function InputField({ game } : { game: string }) {
         name.toLowerCase().includes(query.toLowerCase())
       );
       setMatches(filteredMatches);
+      setNoMatches(true);
     }
   };
 
-  const handleBlur = () => {
-    setIsFocused(false);
-    setMatches([]); 
-  };
-
   const handleSelect = (match: string) => {
+    console.log("asda")
     setQuery(match);
     setMatches([]);
     setIsFocused(false);
+    setNoMatches(true)
   };
 
   // Close dropdown if user clicks outside
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (inputRef.current && !inputRef.current.contains(event.target as Node) && !(event.target instanceof HTMLElement && event.target.closest('.input-results'))) {
-        setMatches([]); 
+        setMatches([]);
+        setNoMatches(false);
       } 
     };
     document.addEventListener('mousedown', handleClickOutside);
@@ -70,7 +70,6 @@ function InputField({ game } : { game: string }) {
           ref={inputRef}
           onChange={handleChange}
           onFocus={handleFocus}
-          onBlur={handleBlur} 
           placeholder="Search for a character..."
           style={{ width: '100%', padding: '8px', fontSize: '14px', border: '1px solid #ccc', borderRadius: '5px' }}
         />
@@ -88,13 +87,13 @@ function InputField({ game } : { game: string }) {
                   className="icon"
                 />
                 { 
-                  <span style={{position: 'absolute', left: '50%', transform: 'translateX(-50%)'}}>{match}</span>
+                  <span style={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>{match}</span>
                 }
               </div>
             ))}
           </div>
         )}
-        {isFocused && matches.length === 0 && query && (
+        {isFocused && matches.length === 0 && query && noMatches && (
           <div 
             className="input-results"
             style={{padding: '4px', cursor: 'pointer', textAlign: 'center', border: '1px black solid' }}
